d1b4f2aa674d504268938aee5d1b1193265ab198
D. MacCarthy <sc8pr.py@gmail.com>
2024-09-06 08:24:28 -0600

    Add files via upload

---
+++ a_pp/p04_ex1.py
@@ -0,0 +1,13 @@
+"""
+CSE 2110: Procedural Programming
+Pre- & Post-Conditions
+Example 1
+
+"""
+
+from resource.wholeNum import isPrime
+
+print(isPrime(6))
+print(isPrime(7))
+print(isPrime(-7))
+print(isPrime("Hi"))
