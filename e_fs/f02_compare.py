0c1fc50dbc6368fd3cf999435a9a0b747c2aaad3
D. MacCarthy <sc8pr.py@gmail.com>
2025-01-22 09:04:40 -0700

    Add files via upload

--- e_fs/f02_compare.py
+++ e_fs/f02_compare.py
@@ -7,16 +7,16 @@ Reading Text Files
 # Complete this function only...
 
 def compare(file1, file2):
-	pass
+    pass
 
 
 # DO NOT EDIT any of the code below.
 
 def main():
-	prompt = "Enter the path to a text file: "
-	file1 = input(prompt)
-	file2 = input(prompt)
-	same = compare(file1, file2)
-	print("The files are {} the same.".format("" if same else "NOT"))
+    prompt = "Enter the path to a text file: "
+    file1 = input(prompt)
+    file2 = input(prompt)
+    same = compare(file1, file2)
+    print("The files are {} the same.".format("" if same else "NOT"))
 
 main()
